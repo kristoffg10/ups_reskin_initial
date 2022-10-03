c385d3dcf9c4a3670154ad1dd35f6e7c112b383c
kristoffg10 <108311554+kristoffg10@users.noreply.github.com>
2022-10-03 13:31:49 +0800

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import "../../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="mt-8">
+        <Col lg="3" md="3">
+            <p className="copyright__text">
+            © Unified Products & Services 2022 
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <center>
+              <p className="copyright__text">
+                <Link to="">
+                  <p>Our Website</p>
+                </Link>
+                <Link to="">
+                  <p>Contact Us</p>
+                </Link>
+                <Link to="">
+                  <p>Help</p>
+                </Link>
+                <Link to="">
+                  <p>Privacy Policy</p>
+                </Link>
+                <Link to="">
+                  <p>Terms & Condition</p>
+                </Link>
+                <Link to="">
+                  <p>Android App</p>
+                </Link>
+                <Link to="">
+                  <p>IOS App</p>
+                </Link>
+              </p>
+            </center>
+          </Col>
+          <Col lg="3" md="3">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <span>
+                {" "}
+                <Link to="https://www.facebook.com/">
+                  <i class="ri-facebook-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                {" "}
+                <Link to="https://www.linkedin.com/">
+                  <i class="ri-linkedin-line"></i>
+                </Link>{" "}
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,93 @@
+import React, { useRef, useEffect } from "react";
+
+import { Container } from "reactstrap";
+import logo from "../../assets/images/UPSlogo.png";
+import { NavLink, Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { cartUiActions } from "../../store/shopping-cart/cartUiSlice";
+
+import "../../styles/header.css";
+
+const nav__links = [
+  {
+    display: "Our Website",
+    path: "/home",
+  },
+  {
+    display: "Help",
+    path: "/foods",
+  },
+];
+
+const Header = () => {
+  const menuRef = useRef(null);
+  const headerRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatch = useDispatch();
+
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
+  const toggleCart = () => {
+    dispatch(cartUiActions.toggle());
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("header__shrink");
+      } else {
+        headerRef.current.classList.remove("header__shrink");
+      }
+    });
+
+    return () => window.removeEventListener("scroll");
+  }, []);
+
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <div className="nav__wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+     
+          </div>
+          <h5 className="page__title">UPS Web Portal V2</h5>
+
+          {/* ======= menu ======= */}
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <div className="menu d-flex align-items-center gap-5">
+              {nav__links.map((item, index) => (
+                <NavLink
+                  to={item.path}
+                  key={index}
+                  className={(navClass) =>
+                    navClass.isActive ? "active__menu" : ""
+                  }
+                >
+                  {item.display}
+                </NavLink>
+              ))}
+            </div>
+          </div>
+
+          {/* ======== nav right icons ========= */}
+          <div className="nav__right d-flex align-items-center gap-4">
+              <button className="btn btn-light btn-block" type="submit" >
+                <Link to="/login">Login</Link>
+              </button>
+
+            <span className="mobile__menu" onClick={toggleMenu}>
+              <i class="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
